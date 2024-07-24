28bcafeb8432326a56c4c236c3b79b18d8b5f561
fijiro <77334716+fijiro@users.noreply.github.com>
2024-07-24 10:28:28 +0300

    Initial Commit

--- src/main/java/project/game/blockchaintycoon/GameController.java
+++ src/main/java/project/game/blockchaintycoon/GameController.java
@@ -1,4 +1,16 @@
 package project.game.blockchaintycoon;
 
-public class GameController {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GameController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
 }
