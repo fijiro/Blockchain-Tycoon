6770e8152536b07b32208ed5bbddedc6c8532851
fijiro <77334716+fijiro@users.noreply.github.com>
2024-08-07 11:02:32 +0300

    Fix warnings and use string templates

--- src/main/java/project/game/blockchaintycoon/GamePanel.java
+++ src/main/java/project/game/blockchaintycoon/GamePanel.java
@@ -20,12 +20,12 @@ public class GamePanel extends Pane {
 
     final int originalTileSize = 16; // 16x16 pixels
     final int scale = 3;
-
-    final int tileSize = originalTileSize * scale; // 48x48 pixels
-    final int maxScreenCol = 16;
-    final int maxScreenRow = 12;
-    /*final int screenWidth = tileSize * maxScreenCol; // 768 pixels
-    final int screenHeight = tileSize * maxScreenRow; // 576 pixels */
+    /*
+        final int tileSize = originalTileSize * scale; // 48x48 pixels
+        final int maxScreenCol = 16;
+        final int maxScreenRow = 12;
+        final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+        final int screenHeight = tileSize * maxScreenRow; // 576 pixels */
     final int screenWidth = 900;
     final int screenHeight = 700;
 
@@ -35,11 +35,11 @@ public class GamePanel extends Pane {
     public int nodet = 1;
     private int upgrades = 1;
 
-    private Canvas canvas;
+    private final Canvas canvas;
     private Timeline gameLoop;
-    private Button shopButton;
-    private Font customFont;
-    private Font customFont2;
+    //private Button shopButton;
+    private final Font customFont;
+    private final Font customFont2;
 
 
     public GamePanel(Stage primaryStage) {
@@ -139,9 +139,9 @@ public class GamePanel extends Pane {
 
     public void update() { // päivittää laskurit ja tulostaa ne
         rahat += (double) rahojenKasvu / 10;
-        System.out.println("Rahat: " + rahat);
-        System.out.println("Asiakkaat: " + asiakkaat);
-        System.out.println("Nodet: " + nodet);
+        System.out.println(STR."Rahat: \{rahat}");
+        System.out.println(STR."Asiakkaat: \{asiakkaat}");
+        System.out.println(STR."Nodet: \{nodet}");
     }
 
     public void render() { // näkyvät laskurit
@@ -152,47 +152,47 @@ public class GamePanel extends Pane {
         gc.setFill(Color.WHITE);
         gc.setFont(customFont);  // fontti ja koko
         DecimalFormat f = new DecimalFormat("##.00");
-        gc.fillText("Money: " + f.format(rahat) + "$", 270, 225);
-        gc.fillText("Customers: " + asiakkaat, 270, 325);
-        gc.fillText("Nodes: " + nodet, 270, 275);
+        gc.fillText(STR."Money: \{f.format(rahat)}$", 270, 225);
+        gc.fillText(STR."Customers: \{asiakkaat}", 270, 325);
+        gc.fillText(STR."Nodes: \{nodet}", 270, 275);
         //Ad button rendering
         if (rahat >= 40 * asiakkaat && nodet > asiakkaat) {
             gc.setFill(Color.GREEN);
             gc.setFont(customFont2);
-            gc.fillText((40 * asiakkaat) + "$", 547, 540);
+            gc.fillText(STR."\{40 * asiakkaat}$", 547, 540);
         }
         else {
             gc.setFill(Color.RED);
             gc.setFont(customFont2);
-            gc.fillText((40 * asiakkaat) + "$", 547, 540);
+            gc.fillText(STR."\{40 * asiakkaat}$", 547, 540);
         }
         //Node button rendering
         if (rahat >= 10 * nodet) {
             gc.setFill(Color.GREEN);
             gc.setFont(customFont2);
-            gc.fillText((10 * nodet) + "$", 333, 540);
+            gc.fillText(STR."\{10 * nodet}$", 333, 540);
         }
         else {
             gc.setFill(Color.RED);
             gc.setFont(customFont2);
-            gc.fillText((10 * nodet) + "$", 333, 540);
+            gc.fillText(STR."\{10 * nodet}$", 333, 540);
         }
         //Upgrade button rendering
         if (rahat >= 100 * upgrades * upgrades) {
             gc.setFill(Color.GREEN);
             gc.setFont(customFont2);
-            gc.fillText((100 * upgrades * upgrades) + "$", 761, 540);
+            gc.fillText(STR."\{100 * upgrades * upgrades}$", 761, 540);
         }
         else {
             gc.setFill(Color.RED);
             gc.setFont(customFont2);
-            gc.fillText((100 * upgrades * upgrades) + "$", 761, 540);
+            gc.fillText(STR."\{100 * upgrades * upgrades}$", 761, 540);
         }
     }
 
-    public void setShopButtonVisibility(boolean visible) {
-        shopButton.setVisible(visible);
-    }
+//    public void setShopButtonVisibility(boolean visible) {
+//        shopButton.setVisible(visible);
+//    }
 
     public void setRahojenKasvu(int newValue) {
         this.rahojenKasvu = newValue;
